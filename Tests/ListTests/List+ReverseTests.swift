2a50499e88a5800cd4e61c826992b8fd3758b4f2
Alexander Young <alex.buzov@icloud.com>
2022-07-17 10:03:30 -0700

    Update List+ReverseTests.swift

--- Tests/ListTests/List+ReverseTests.swift
+++ Tests/ListTests/List+ReverseTests.swift
@@ -10,20 +10,9 @@ final class ListReverseTests: XCTestCase {
     func testEmptyListReverse() {
         XCTAssertEqual(List<Int>.empty.reversed, .empty)
     }
-    
-    func testPerformanceReverse() {
-
-        let list = List(repeating: 1, count: 1000)
-        
-        measure {
-            let reversedList = list.reversed
-            print(reversedList.length)
-        }
-    }
 
     static var allTests = [
         ("testListReverse", testListReverse),
         ("testEmptyListReverse", testEmptyListReverse),
-        ("testPerformanceReverse", testPerformanceReverse)
     ]
 }
